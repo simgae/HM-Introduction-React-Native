2317b6d6e44f4fc1341022e82362497624d2b782
Simon Gaertner <simongaertner@gmx.net>
2025-04-12 13:15:30 +0200

    Add docs

--- src/context/RecipeProvider.tsx
+++ src/context/RecipeProvider.tsx
@@ -6,6 +6,15 @@ export default function RecipeProvider({
 }: {
   children: React.ReactNode;
 }) {
+  // NOTE: The main usage of a provider in React is to provide a context to its children.
+  // But by doing it this way, we create only a read access to the data behind the context.
+  // @see:  https://react.dev/reference/react/useContext#passing-data-deeply-into-the-tree
+  // Yes, we cann call `setRecipe` from the context, but the change will not be reflected in the provider.
+
+  // To reflect the changes in the provider, we need to use a state and pass it down to the context.
+  // This way, we can use the context to read the data and the provider to update it.
+  // @see: https://react.dev/reference/react/useContext#updating-data-passed-via-context
+
   return (
     <RecipeContext.Provider value={useContext(RecipeContext)}>
       {children}
